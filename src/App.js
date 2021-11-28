bb86fca457786e7d18924abb1b7af0a666f6e249
Lucien <lucien.maillard@gmail.com>
2021-11-28 23:49:04 +0100

    modification of the structure of the code

--- src/App.js
+++ src/App.js
@@ -1,217 +1,284 @@
-import React, { useState, Fragment } from "react";
-import axios from "axios";
-import { nanoid } from "nanoid";
-import "./App.css";
-import data from "./mock-data.json";
-import ReadOnlyRow from "./components/ReadOnlyRow";
-import EditableRow from "./components/EditableRow";
+import './App.css';
+import React from "react";
+import {makeStyles} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import {
+    Box,
+    Button,
+    Dialog, DialogActions,
+    DialogContentText,
+    DialogTitle,
+    Divider,
+    Grid,
+    TextField,
+    Typography
+} from "@material-ui/core";
+import {uuid} from 'uuidv4';
 
-const App = () => {
-  const [books, setBooks] = useState(data);
-  const [addFormData, setAddFormData] = useState({
-    title: "",
-    author: "",
-    category: "",
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+    paper: {
+        width: "100%",
+        height: "100vh",
+        margin: "auto"
+    },
+    dialog: {
+        padding: "5px",
+    },
+    divider: {
+        color: "red",
+    },
+    title: {
+        color: "black",
+        backgroundColor: "white"
+    }
 
-  });
+});
 
-  const [editFormData, setEditFormData] = useState({
-    title: "",
-    author: "",
-    category: "",
-  });
-
-  const [editBookId, setEditBookId] = useState(null);
-
-  const handleAddFormChange = (event) => {
-    event.preventDefault();
-
-    const fieldName = event.target.getAttribute("name");
-    const fieldValue = event.target.value;
-
-    const newFormData = { ...addFormData };
-    newFormData[fieldName] = fieldValue;
-
-    setAddFormData(newFormData);
-  
-  };
-
-  const handleEditFormChange = (event) => {
-    event.preventDefault();
-
-    const fieldName = event.target.getAttribute("name");
-    const fieldValue = event.target.value;
-
-    const newFormData = { ...editFormData };
-    newFormData[fieldName] = fieldValue;
-
-    setEditFormData(newFormData);
-  };
-
-  const handleAddFormSubmit = (event) => {
-    event.preventDefault();
-
-    const newBook = {
-      id: nanoid(),
-      title: addFormData.title,
-      author: addFormData.author,
-      category: addFormData.category
-    };
-
-    const newBooks = [...books, newBook];
-    setBooks(newBooks);
-    axios({
-      method: 'POST',
-      url: 'https://rcyg32ptue.execute-api.us-east-2.amazonaws.com/',
-      data: newBooks
-  })
-  .then(function (reponse) {
-      //On traite la suite une fois la réponse obtenue 
-      console.log(reponse);
-  })
-  .catch(function (erreur) {
-      //On traite ici les erreurs éventuellement survenues
-      console.log(erreur);
-  }
-  )};
-
-  function handleEditFormSubmit(event) {
-    event.preventDefault();
-
-    const editedBook = {
-      id: editBookId,
-      title: editFormData.title,
-      author: editFormData.author,
-      category: editFormData.category
-    };
-
-    const newBooks = [...books];
-
-    const index = books.findIndex((book) => book.id === editBookId);
-
-    newBooks[index] = editedBook;
-
-    setBooks(newBooks);
-    setEditBookId(null);
-
-    axios({
-      method: 'POST',
-      url: 'https://rcyg32ptue.execute-api.us-east-2.amazonaws.com/',
-      data: newBooks
-  })
-  .then(function (reponse) {
-      //On traite la suite une fois la réponse obtenue 
-      console.log(reponse);
-  })
-  .catch(function (erreur) {
-      //On traite ici les erreurs éventuellement survenues
-      console.log(erreur);
-  })
-  };
-
-  const handleEditClick = (event, book) => {
-    event.preventDefault();
-    setEditBookId(book.id);
-
-    const formValues = {
-      title: book.title,
-      author: book.author,
-      category: book.category,
-    };
-
-    setEditFormData(formValues);
-  };
-
-  const handleCancelClick = () => {
-    setEditBookId(null);
-  };
-
-  const handleDeleteClick = (bookId) => {
-    const newBooks = [...books];
-
-    const index = books.findIndex((book) => book.id === bookId);
-
-    newBooks.splice(index, 1);
-
-    setBooks(newBooks);
-
-    axios({
-      method: 'DELETE',
-      url: 'https://rcyg32ptue.execute-api.us-east-2.amazonaws.com/',
-      data: newBooks
-  })
-  .then(function (reponse) {
-      //On traite la suite une fois la réponse obtenue 
-      console.log(reponse);
-  })
-  .catch(function (erreur) {
-      //On traite ici les erreurs éventuellement survenues
-      console.log(erreur);
-  });
-  };
-
-  return (
-    <div className="app-container">
-      <form onSubmit={handleEditFormSubmit}>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author</th>
-              <th>Category</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {books.map((book) => (
-              <Fragment>
-                {editBookId === book.id ? (
-                  <EditableRow
-                    editFormData={editFormData}
-                    handleEditFormChange={handleEditFormChange}
-                    handleCancelClick={handleCancelClick}
-                  />
-                ) : (
-                  <ReadOnlyRow
-                    book={book}
-                    handleEditClick={handleEditClick}
-                    handleDeleteClick={handleDeleteClick}
-                  />
-                )}
-              </Fragment>
-            ))}
-          </tbody>
-        </table>
-      </form>
-
-      <h2>Add a book</h2>
-      <form onSubmit={handleAddFormSubmit}>
-        <input
-          type="text"
-          name="title"
-          required="required"
-          placeholder="Enter a title..."
-          onChange={handleAddFormChange}
-        />
-        <input
-          type="text"
-          name="author"
-          required="required"
-          placeholder="Enter an author..."
-          onChange={handleAddFormChange}
-        />
-        <input
-          type="text"
-          name="category"
-          required="required"
-          placeholder="Enter a category ..."
-          onChange={handleAddFormChange}
-        />
-        
-        <button type="submit">Add</button>
-      </form>
-    </div>
-  );
+const headers = {
+    "Content-Type": "application/json",
+    "accept": "application/json",
 };
+
+const baseUrl = "https://p60c8tz94d.execute-api.eu-west-3.amazonaws.com/books";
+const cleanBook = {
+    id: null,
+    author: "",
+    title: "",
+    category: "",
+}
+
+function App() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [books, setBooks] = React.useState([]);
+    const [newBook, setNewBook] = React.useState({
+        author: "",
+        title: "",
+        category: "",
+    });
+    const [updatedBook, setUpdatedBook] = React.useState({...cleanBook});
+
+    
+    React.useEffect(async () => {  
+        const requestOptions = {
+            method: "GET",
+            headers: headers
+        };
+        const response = await (await fetch(baseUrl, requestOptions)).json();
+        setBooks(response.Items);
+    }, []);
+  
+
+    const saveBook = async () => {
+        const id = uuid();
+        const requestOptions = {
+            method: "PUT",
+            headers: headers,
+            body: JSON.stringify({
+                ...newBook,
+                id: id,
+            })
+        };
+        try {
+            await (await fetch(baseUrl, requestOptions)).json();
+            setBooks([...books, {
+                ...newBook,
+                id: id,
+            }]);
+        } catch (err) {
+            alert("Oups ... error saving new book: " + err.message);
+        }
+    }
+
+    const updateBook =async () => {
+        const requestOptions = {
+            method: "PUT",
+            headers: headers,
+            body: JSON.stringify({
+                ...updatedBook,
+            })
+        };
+        try {
+            await (await fetch(baseUrl, requestOptions)).json();
+            const index = books.findIndex(c => c.id === updatedBook.id);
+            const booksNew = [...books];
+            booksNew[index] = {...updatedBook};
+            setBooks(booksNew);
+        } catch(err) {
+            alert("Oups ... error updating new book: " + err.message);
+        }
+    }
+
+    const getPopup = () => {
+        if (!open) return null;
+        const close = () => {
+            setUpdatedBook({...cleanBook});
+            setOpen(false);
+        }
+        const update = () => {
+            updateBook().then(_ => close());
+        }
+        return (
+            <Dialog className={classes.dialog} fullWidth={true} open={open} aria-labelledby="company-dialog-popup">
+                <DialogTitle className={classes.title}>
+                    {"New Car"}
+                </DialogTitle>
+                <Divider classes={{
+                    root: classes.divider
+                }}/>
+                <Box mr={2} ml={2} mt={0}>
+                    <DialogContentText style={{marginTop: "20px"}}>
+                        {"Updating book's details below"}
+                    </DialogContentText>
+                        <Grid item>
+                            <TextField
+                                onChange={(event) =>
+                                    setUpdatedBook({...updatedBook, author: event.target.value})}
+                                autoFocus
+                                id="name"
+                                label={"Author"}
+                                type="text"
+                                fullWidth
+                                value={updatedBook.author}
+                            />
+                        </Grid>
+                        <Grid item>
+                        <TextField
+                                onChange={(event) =>
+                                    setUpdatedBook({...updatedBook, title: event.target.value})}
+                                autoFocus
+                                id="name"
+                                label={"Title"}
+                                type="text"
+                                fullWidth
+                                value={updatedBook.title}
+                            />
+                        </Grid>
+                        <Grid item>
+                        <TextField
+                                onChange={(event) =>
+                                    setUpdatedBook({...updatedBook, category: event.target.value})}
+                                autoFocus
+                                id="name"
+                                label={"Category"}
+                                type="text"
+                                fullWidth
+                                value={updatedBook.category}
+                            />
+                    </Grid>
+                </Box>
+                <DialogActions style={{marginTop: "20px"}}>
+                    <Button
+                        variant={"filled"}
+                        onClick={close}
+                        color="secondary"
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        variant={"filled"}
+                        onClick={update}
+                        color="primary">
+                        Update
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        )
+    }
+
+    const deleteBook = async (id) => {
+        const requestOptions = {
+            method: "DELETE",
+            headers: headers,
+        };
+        try {
+            await (await fetch(`${baseUrl}\\${id}`, requestOptions));
+            setBooks(books.filter(c => c.id !== id));
+        } catch (err) {
+            alert("Oups ... error when deleting the book: " + err.message);
+        }
+    }
+
+    return (
+        <React.Fragment>
+            <Grid direction={"column"} className={classes.paper} container justifyContent="center"
+                  alignItems={"center"}>
+                <Grid item><Typography align={"center"} gutterBottom variant="h2">Our Books Table</Typography></Grid>
+                <Grid item>
+                    <Paper>
+                        <TableContainer style={{maxHeight: "70vh"}} component={Paper}>
+                            <Table stickyHeader aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align="center">Author</TableCell>
+                                        <TableCell align="center">Title</TableCell>
+                                        <TableCell align="center">Category</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {books.map(c => (
+                                        <TableRow key={c.id}>
+                                            <TableCell component="th" scope={"row"}>
+                                                {c.modelname}
+                                            </TableCell>
+                                            <TableCell align={"center"}>{c.color}</TableCell>
+                                            <TableCell align={"center"}>{c.productionyear}</TableCell>
+                                            <TableCell align={"center"}>{c.price}</TableCell>
+                                            <TableCell align={"center"}>
+                                                <Button onClick={() => {
+                                                    setUpdatedBook({...c});
+                                                    setOpen(true);
+                                                }}>
+                                                    Update
+                                                </Button>
+                                                <Button onClick={() => deleteBook(c.id)}>
+                                                    Delete
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <TableRow>
+                            <TableCell align={'center'}>
+                                <TextField id="new_author" label="New author" value={newBook.author}
+                                           onChange={(e) =>
+                                               setNewBook({...newBook, modelname: e.target.value})}
+                                />
+                            </TableCell>
+                            <TableCell align={'center'}>
+                                <TextField id="new_author" label="New title" value={newBook.title}
+                                           onChange={(e) =>
+                                               setNewBook({...newBook, modelname: e.target.value})}
+                                />
+                            </TableCell>
+                           
+                            <TableCell align={'center'}>
+                                <TextField id="new_author" label="New category" value={newBook.title}
+                                           onChange={(e) =>
+                                               setNewBook({...newBook, modelname: e.target.value})}
+                                />
+                            </TableCell>
+                            <TableCell align={"center"}>
+                                <Button onClick={saveBook}>Save</Button>
+                            </TableCell>
+                        </TableRow>
+                    </Paper>
+                </Grid>
+            </Grid>
+            {getPopup()}
+        </React.Fragment>
+    );
+}
 
 export default App;
